7917f86d4b1d87c16931f06d15be8389cc342335
Terence <wrze@sina.com>
2017-07-24 23:25:46 +0800

    add desc vue

---
+++ src/components/Description.vue
@@ -0,0 +1,33 @@
+<!--Description vue-->
+<template>
+	<div class="wrapper m1">
+		知乎Description
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ZhihuHeadFix',
+		data() {
+			return {
+
+			}
+		},
+		created: function() {
+
+		},
+		methods: {
+			click: function() {
+				console.log('back');
+				history.back();
+			},
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	body {
+		text-align: center;
+	}
+</style>
